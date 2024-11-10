c859b115d0adf685a92a053847ba91209a59fb47
Josphat Baraka Mramba <119582122+BullionVAN380@users.noreply.github.com>
2024-11-10 13:16:23 +0300

    Update App.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Components/Login/Login';
 import Dashboard from './Components/Dashboard/Dashboard';
 import ClearanceForm from './Components/ClearanceForm/ClearanceForm';
 import DocumentCollection from './Components/DocumentCollection/DocumentCollection';
-
-
 
 const App = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Login />} />
-                <Route path="/dashboard" element={<Dashboard  />} />
+                <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/clearance/:departmentId" element={<ClearanceForm />} />
                 <Route path="/documents" element={<DocumentCollection />} />
-                
-                {/* Add more routes as needed */}
+                <Route path="*" element={<Login />} />  {/* Fallback route */}
             </Routes>
         </Router>
     );
